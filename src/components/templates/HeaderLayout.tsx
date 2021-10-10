49df3017b63b48b3393f559eba560a364773e32a
hoshico <77495217+hoshico@users.noreply.github.com>
2021-10-10 13:30:18 +0900

    mk Header

---
+++ src/components/templates/HeaderLayout.tsx
@@ -0,0 +1,16 @@
+import { memo, ReactNode, VFC } from "react";
+import { Header } from "../organisms/layout/Header";
+
+type Props = {
+  childre: ReactNode;
+};
+
+export const HeaderLayout: VFC<Props> = memo((props) => {
+  const { children } = props;
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+});
