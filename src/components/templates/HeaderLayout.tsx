1e711afad8769c53764d5d3b28c732154fecad83
hoshico <77495217+hoshico@users.noreply.github.com>
2021-10-11 08:20:00 +0900

    made drawer

--- src/components/templates/HeaderLayout.tsx
+++ src/components/templates/HeaderLayout.tsx
@@ -2,7 +2,7 @@ import { memo, ReactNode, VFC } from "react";
 import { Header } from "../organisms/layout/Header";
 
 type Props = {
-  childre: ReactNode;
+  children: ReactNode;
 };
 
 export const HeaderLayout: VFC<Props> = memo((props) => {
